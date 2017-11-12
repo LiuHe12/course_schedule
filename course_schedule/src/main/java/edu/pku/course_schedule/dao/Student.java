7b571278da21f1051506443f759da1897cbe2b9a
User <User@192.168.31.1>
2017-11-12 19:39:10 +0800

    11/12 CY upload

--- course_schedule/src/main/java/edu/pku/course_schedule/dao/Student.java
+++ course_schedule/src/main/java/edu/pku/course_schedule/dao/Student.java
@@ -1,5 +1,11 @@
-package edu.pku.course_schedule.entity;
+package edu.pku.course_schedule.dao;
 
 public class Student {
+	
+	private String id;
+	
+	private String name;
+	
+	
 
 }
--- course_schedule/src/main/java/edu/pku/course_schedule/dao/Teacher.java
+++ course_schedule/src/main/java/edu/pku/course_schedule/dao/Teacher.java
@@ -1,5 +1,67 @@
-package edu.pku.course_schedule.entity;
+package edu.pku.course_schedule.dao;
 
 public class Teacher {
+	
+	/*
+	 * 到時再改成和資料庫欄位名稱一樣
+	 * */
+	
+	private String id;
+	
+	private String name;
+	
+	private int basicCourseHours;
+	
+	private double salaryBasic;
 
+	
+//	private double salaryListen;
+	
+//	private double salarySpeak;
+	
+//	private double salaryRead;
+	
+//	private double salaryWrite;
+	
+	
+	
+	
+	/**********Getter and Setter**********/
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBasicCourseHours() {
+		return basicCourseHours;
+	}
+
+	public void setBasicCourseHours(int basicCourseHours) {
+		this.basicCourseHours = basicCourseHours;
+	}
+
+	public double getSalaryBasic() {
+		return salaryBasic;
+	}
+
+	public void setSalaryBasic(double salaryBasic) {
+		this.salaryBasic = salaryBasic;
+	}
+	
+
+
+	
+	
 }
