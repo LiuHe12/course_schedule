fc9ff9f77aa65616de346aa194951ca44aa2dbdf
Haylee\_Haylee <962227369@qq.com>
2017-11-14 22:42:15 +0800

    增加数据库连接池工具

--- src/main/java/edu/pku/course_schedule/services/JdbcPool.java
+++ src/main/java/edu/pku/course_schedule/services/JdbcPool.java
@@ -1,5 +1,155 @@
 package edu.pku.course_schedule.services;
 
-public class JdbcPool {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.LinkedList;
+import java.util.Properties;
 
+import javax.sql.DataSource;
+import java.util.logging.Logger;
+
+
+public class JdbcPool implements DataSource{
+	
+	private  String driver;
+	private  String url;
+	private  String username;
+	private  String password;
+	private  int maxPoolSize;
+	
+	private  LinkedList<Connection> listConnections = new LinkedList<Connection>();
+	public  boolean loadDBconf(String confFile) {
+		FileInputStream inputStream = null;
+		try {
+			inputStream = new FileInputStream(new File(confFile));
+			Properties p = new Properties();
+			p.load(inputStream);
+			driver=p.getProperty("driver");
+			url=p.getProperty("url");
+			username=p.getProperty("username");
+			password=p.getProperty("password");
+			maxPoolSize=Integer.parseInt(p.getProperty("maxPoolSize"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+			
+		}
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "JdbcPool [driver=" + driver + ", url=" + url + ", username=" + username + ", password=" + password
+				+ ", maxPoolSize=" + maxPoolSize + "]";
+	}
+	public String getDriver() {
+		return driver;
+	}
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getMaxPoolSize() {
+		return maxPoolSize;
+	}
+	public void setMaxPoolSize(int maxPoolSize) {
+		this.maxPoolSize = maxPoolSize;
+	}
+
+
+	public LinkedList<Connection> getListConnections() {
+		return listConnections;
+	}
+	public void setListConnections(LinkedList<Connection> listConnections) {
+		listConnections = listConnections;
+	}
+	@Override
+	public PrintWriter getLogWriter() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public int getLoginTimeout() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void setLogWriter(PrintWriter arg0) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setLoginTimeout(int arg0) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public boolean isWrapperFor(Class<?> arg0) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public <T> T unwrap(Class<T> arg0) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Connection getConnection() throws SQLException {
+		//如果数据库连接池中的连接对象的个数大于0
+		if (listConnections.size()>0) {
+			return listConnections.removeFirst();
+		}else {
+			throw new RuntimeException("对不起，数据库忙");
+		}
+	}
+
+	@Override
+	public Connection getConnection(String username, String password) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
---
+++ src/main/java/edu/pku/course_schedule/services/JdbcUtil.java
@@ -0,0 +1,62 @@
+package edu.pku.course_schedule.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.taglibs.standard.tag.common.xml.IfTag;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JdbcUtil {
+	static JdbcPool jdbcPool=new JdbcPool();
+	private static Logger logger=Logger.getLogger(JdbcUtil.class);
+	static {
+		init();
+		try {
+			Class.forName(jdbcPool.getDriver());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for(int i=0;i<jdbcPool.getMaxPoolSize();i++) {
+			Connection conn;
+			try {
+				conn = DriverManager.getConnection(jdbcPool.getUrl(), jdbcPool.getUsername(), jdbcPool.getPassword());
+				jdbcPool.getListConnections().add(conn);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		logger.info("数据池链初始化成功~");
+		
+	}
+	public static void init() {
+		boolean loaddb=jdbcPool.loadDBconf("db.properties");
+		if(!loaddb) {
+			logger.error("读取数据库配置文件失败！");;
+		}
+	}
+	public static synchronized Connection getConnection() throws SQLException {
+		return jdbcPool.getConnection();
+	}
+	public static synchronized void releaseConn(Connection conn) throws SQLException {
+		jdbcPool.getListConnections().add(conn);
+	}
+	public static void release(Statement statement,ResultSet rs,Connection conn) throws SQLException {
+		if(rs!=null) {
+			rs.close();
+		}
+		if(statement!=null) {
+			statement.close();
+		}
+		releaseConn(conn);
+	}
+}
