14a288d6bf2ab8e5d1bacc5f4a8b88c2721ff30c
User <User@LAPTOP-R84IC4C7>
2017-11-18 01:22:06 +0800

    Add student_course entity

---
+++ src/main/java/edu/pku/course_schedule/dao/entity/Student_course.java
@@ -0,0 +1,52 @@
+package edu.pku.course_schedule.dao.entity;
+
+public class Student_course {
+	
+	private String course_name;
+	
+	private String teacher_id;
+	
+	private String student_id;
+	
+	private int price;
+	
+	
+	
+	
+	/**********Getter and Setter**********/
+
+	public String getCourse_name() {
+		return course_name;
+	}
+
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+
+	public String getTeacher_id() {
+		return teacher_id;
+	}
+
+	public void setTeacher_id(String teacher_id) {
+		this.teacher_id = teacher_id;
+	}
+
+	public String getStudent_id() {
+		return student_id;
+	}
+
+	public void setStudent_id(String student_id) {
+		this.student_id = student_id;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+}
